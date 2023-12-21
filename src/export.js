544032f9ab292881631698699000b89f2ff62948
Bernard Niset <bernard.niset@gmail.com>
2023-12-21 20:11:15 +0100

    feat: Update build workflow and export PDF functionality

--- src/export.js
+++ src/export.js
@@ -1,29 +1,43 @@
-import puppeteer from 'puppeteer';
-import fileUrl from 'file-url';
-import dayjs from 'dayjs';
+import puppeteer from "puppeteer";
+import fileUrl from "file-url";
+import dayjs from "dayjs";
+import { copyFile } from "copy-file";
+
+const DIST_HTML = "./dist/index.html";
+const DIST_PDF = "dist/Bernard Niset.pdf";
+const DIST_PDF_WITH_DATE = `dist/Bernard Niset ${dayjs().format("YYYY-MM-DD")}.pdf`;
 
 export const exportPDF = async () => {
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: 'new',
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: "new",
   });
   const page = await browser.newPage();
-  const url = fileUrl(`./dist/index.html`);
-  await page.goto(url, { waitUntil: 'networkidle0' });
-  await page.waitForSelector('main');
+  const url = fileUrl(DIST_HTML);
+  await page.goto(url, { waitUntil: "networkidle0" });
+  await page.waitForSelector("main");
   await page.pdf({
-    path: `dist/Bernard Niset ${dayjs().format('YYYY-MM-DD')}.pdf`,
-    format: 'A4',
+    path: DIST_PDF,
+    format: "A4",
     printBackground: true,
     landscape: false,
     margin: {
-      top: '15mm',
-      right: '10mm',
-      bottom: '15mm',
-      left: '10mm',
+      top: "15mm",
+      right: "10mm",
+      bottom: "15mm",
+      left: "10mm",
     },
   });
   await browser.close();
 };
 
-exportPDF().catch(console.error);
+export const copyPDF = async () => {
+  await copyFile(DIST_PDF, DIST_PDF_WITH_DATE);
+};
+
+const main = async () => {
+  await exportPDF();
+  await copyPDF();
+};
+
+main().catch(console.error);
