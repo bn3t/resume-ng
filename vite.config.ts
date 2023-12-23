4500bd9bc62ad40e9b01c249ce6ab5b7e84a8422
Bernard Niset <bernard.niset@gmail.com>
2023-12-23 15:22:24 +0100

    feat: add open graph support into the html

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,10 @@ export default defineConfig({
           .replace(
             "<!-- meta-->",
             `
-            <meta property="og:title" content="${resume.basics.name}" />
-            <meta property="og:description" content="${resume.basics.label}" />
+            <meta property="og:title" content="${resume.basics.name} - ${resume.basics.label}" />
+            <meta property="og:author" content="${resume.basics.name}" />
+            <meta property="og:description" content="${resume.basics.summary}" />
+            <meta property="og:image" content="og-image.png" />
             <meta property=“og:type” content=“profile” />
             <meta property="author" content="${resume.basics.name}" />
             <title>${resume.basics.name} - ${resume.basics.label}</title>
