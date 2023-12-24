6a2e29bb95d99f795bd3e2eb4d733fc3a6a18290
Bernard Niset <bn3t@users.noreply.github.com>
2023-12-24 11:05:57 +0100

    feat: Add a download button on web

--- src/App.tsx
+++ src/App.tsx
@@ -24,16 +24,32 @@ function App() {
     document.title = `${processedResume.basics.name} - ${processedResume.basics.label}`;
   }, []);
   return (
-    <main className="mx-auto space-y-4 p-16 shadow-lg print:m-0 print:w-full print:p-0 print:shadow-none sm:w-full lg:w-[800px]">
-      <Page0 basics={processedResume.basics} skills={processedResume.skills} current={processedResume.current} />
-      <div className="break-before-page">
-        <Page1 work={processedResume.page1} />
-      </div>
-      <div className="break-before-page">
-        <Page2 work={processedResume.page2} />
-        <Education education={processedResume.education} />
-      </div>
-    </main>
+    <>
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 shadow-sm print:hidden lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex flex-1 justify-end">
+          <a
+            href={`${resume.basics.name}.pdf`}
+            download
+            className="rounded-lg border border-smo-blue-700  bg-smo-blue-100 px-4 py-2 text-smo-blue-700"
+          >
+            Download as PDF
+          </a>
+        </div>
+      </nav>
+      <main className="mx-auto space-y-4 p-16 shadow-lg print:m-0 print:w-full print:p-0 print:shadow-none sm:w-full lg:w-[800px]">
+        <Page0 basics={processedResume.basics} skills={processedResume.skills} current={processedResume.current} />
+        <div className="break-before-page">
+          <Page1 work={processedResume.page1} />
+        </div>
+        <div className="break-before-page">
+          <Page2 work={processedResume.page2} />
+          <Education education={processedResume.education} />
+        </div>
+      </main>
+    </>
   );
 }
 
