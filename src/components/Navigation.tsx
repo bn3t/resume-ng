7b508cb5059b44f987a0df2f7bcda8dd4c22966f
Bernard Niset <bn3t@users.noreply.github.com>
2024-01-13 11:49:18 +0100

    fix: move gh icon to the left and layout improvements (#15)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,18 +8,14 @@ interface NavigationProps {
 const Navigation = ({ name, githubUrl }: NavigationProps) => {
   return (
     <nav
-      className="mx-auto flex max-w-7xl items-center justify-between p-6 shadow-sm print:hidden lg:px-8"
-      aria-label="Global"
+      className="sticky top-0 mx-auto mb-4 flex max-w-7xl items-center justify-between bg-white p-6 shadow-md print:hidden lg:px-8"
+      aria-label="Navigation"
     >
-      <div className="flex flex-1 items-center justify-end space-x-4">
-        <a
-          href={githubUrl}
-          className="h-[25px] w-[25px] rounded-full bg-smo-blue-700 p-2 text-white"
-          alt="View this project on github"
-        >
-          <Github className="h-6 w-6 translate-y-[1px]" />
-          <span className="sr-only">View this project on github</span>
-        </a>
+      <a href={githubUrl} className="h-[25px] w-[25px] rounded-full bg-smo-blue-700 p-2 text-white">
+        <Github className="h-6 w-6 translate-y-[1px]" />
+        <span className="sr-only">View this project on github</span>
+      </a>
+      <div className="flex items-center justify-end space-x-4">
         <a
           href={`${name}.pdf`}
           download
