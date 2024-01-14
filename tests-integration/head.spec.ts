f69dde882903285cb4a77a408015e3aaefa8609c
Bernard Niset <bn3t@users.noreply.github.com>
2024-01-14 10:04:12 +0100

    chore: add integration testing with playwright (#18)

---
+++ tests-integration/head.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+
+const { describe } = test;
+
+describe("Resume Head", () => {
+  test("has title", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    await expect(page).toHaveTitle(/Bernard Niset - Senior Frontend Developer \(Freelance\)/);
+  });
+
+  test("has open graph", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    await expect(page.locator('meta[property="og:title"]')).toHaveAttribute(
+      "content",
+      "Bernard Niset - Senior Frontend Developer (Freelance)",
+    );
+    await expect(page.locator('meta[property="og:description"]')).toHaveAttribute(
+      "content",
+      /As a Senior Software Engineer, I bring an extensive background in Java-based/,
+    );
+    await expect(page.locator('meta[property="og:image"]')).toHaveAttribute("content", "/og-image.png");
+    await expect(page.locator('meta[property="og:type"]')).toHaveAttribute("content", "profile");
+  });
+
+  test("has meta info", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    await expect(page.locator('meta[name="author"]')).toHaveAttribute("content", "Bernard Niset");
+    await expect(page.locator('meta[name="description"]')).toHaveAttribute(
+      "content",
+      "Senior Frontend Developer (Freelance)",
+    );
+  });
+});
