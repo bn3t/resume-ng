2530b247c06caaaa12ddf1eff861a3b41a0b2a8e
Bernard Niset <bn3t@users.noreply.github.com>
2024-02-09 23:53:53 +0100

    feat: Improve to summary of frontend missions (#28)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,17 +3,25 @@ import { Github } from "lucide-react";
 interface NavigationProps {
   name: string;
   githubUrl: string;
+  version: string;
 }
 
-const Navigation = ({ name, githubUrl }: NavigationProps) => (
+const Navigation = ({ name, githubUrl, version }: NavigationProps) => (
   <nav
     className="sticky top-0 mx-auto mb-4 flex max-w-7xl items-center justify-between bg-white p-6 shadow-md dark:bg-slate-900 dark:shadow-dark-md print:hidden lg:px-8"
     aria-label="Navigation"
   >
-    <a href={githubUrl} className="h-[25px] w-[25px] rounded-full bg-smo-blue-700 p-2 text-white dark:bg-smo-blue-900">
-      <Github className="h-6 w-6 translate-y-[1px]" />
-      <span className="sr-only">View this project on github</span>
-    </a>
+    <div className="flex flex-row items-center space-x-4">
+      <a
+        href={githubUrl}
+        className="h-[25px] w-[25px] rounded-full bg-smo-blue-700 p-2 text-white dark:bg-smo-blue-900"
+      >
+        <Github className="h-6 w-6 translate-y-[1px]" />
+        <span className="sr-only">View this project on github</span>
+      </a>
+      <div className="text-smo-blue-600 dark:text-smo-blue-400">Version: {version}</div>
+    </div>
+
     <div className="flex items-center justify-end space-x-4">
       <a
         href={`${name}.pdf`}
